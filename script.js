c17f09c71d0e8b6400cacfe536524fe45a25c8d6
sirin-acciojob-26 <166235930+sirin-acciojob-26@users.noreply.github.com>
2024-08-08 22:08:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let timerDisplay = document.gutElementById("timer");
+
+let currentDate = new Date();
+
+setInterval(){
+	function(){
+		currentDate = new Date();
+		timerDisplay.innerHTML = currentDate.toLocalString
+	},1000
+}
